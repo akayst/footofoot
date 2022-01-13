c26d6a7ea6cffa1678efb9f5f704be74025af6b5
akay <akayst@naver.com>
2022-01-13 16:32:20 +0900

    test file

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+source 'https://github.com/CocoaPods/Specs.git'
+target 'walkingApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for walkingApp
+  pod 'NMapsMap'
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+  post_install do |pi|
+      pi.pods_project.targets.each do |t|
+          t.build_configurations.each do |bc|
+             bc.build_settings['ARCHS[sdk=iphonesimulator*]'] =  `uname -m`
+          end
+      end
+   end
+end
--- walkingApp/ViewController.swift
+++ walkingApp/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import NMapsMap
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, NMFMapViewTouchDelegate{
+    @IBOutlet var mapView: NMFMapView!
+    var locationManager = CLLocationManager()
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLoc = locations.last?.coordinate
+        let lastLat = lastLoc?.latitude
+        let lastLon = lastLoc?.longitude
+        
+        print("현재 갱신 \(lastLoc)")
+        
+    }
+    var pathArr:Array<AnyObject> = []
+    func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+        print("해당위치의 위도는 \(latlng.lat),\(latlng.lng)")
+        pathArr.append(latlng)
+        print(pathArr)
+        
+        let pathOverlay = NMFPath()
+        let path = pathOverlay.path
+        pathOverlay.path = NMGLineString(points: pathArr)
+        pathOverlay.mapView = mapView
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.positionMode = .compass
+        mapView.touchDelegate = self
+        
+        
+        
+       
+        
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            print("현재 서비스 on 상태")
+            locationManager.startUpdatingLocation()
+            print("현재위치 : \(locationManager.location?.coordinate)")
+            let currentLat = (locationManager.location?.coordinate.latitude)!
+            let currentLon = (locationManager.location?.coordinate.longitude)!
+            print(currentLon)
+            
+        }
+        else{
+            print("현재 위치정보 서비스 off 상태")
+        }
     }
-
-
 }
 
