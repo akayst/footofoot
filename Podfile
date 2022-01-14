150c4b2c9ce25eb76c6782d314488f18be5e45bd
akay <akayst@naver.com>
2022-01-14 20:14:36 +0900

    2022.1.14 지도파트 스케치 메뉴 제작 및 firebase 연동, 바텀시트 라이브러리 적용

--- Podfile
+++ Podfile
@@ -9,6 +9,10 @@ target 'walkingApp' do
   pod 'NMapsMap'
   pod 'Alamofire'
   pod 'SwiftyJSON'
+  pod 'Floaty', '~> 4.2.0'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  pod 'FloatingPanel'
   post_install do |pi|
       pi.pods_project.targets.each do |t|
           t.build_configurations.each do |bc|
---
+++ walkingApp/myViewController.swift
@@ -0,0 +1,18 @@
+//
+//  myViewController.swift
+//  walkingApp
+//
+//  Created by akay on 2022/01/14.
+//
+
+import Foundation
+import UIKit
+import FloatingPanel
+
+class myViewController : UIViewController{
+    @IBOutlet var myTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
