98be49ea6fd2e22d6e3e92e79933448375190934
johnjeongukhur <johnjeongukhur@gmail.com>
2022-01-16 16:44:56 +0900

    refactoring/folder feat/add-loginVC,loginVCStoryboard

---
+++ walkingApp/Controller/Login/LoginVC.swift
@@ -0,0 +1,17 @@
+//
+//  LoginVC.swift
+//  walkingApp
+//
+//  Created by John Hur on 2022/01/16.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
