29af8b00f692a1dcfc8eae1aa9860478549f1ccc
johnjeongukhur <johnjeongukhur@gmail.com>
2022-01-17 19:58:12 +0900

    feat/add-registerVC, and all Scroll View

---
+++ walkingApp/Controller/Login/RegisterVC.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterVC.swift
+//  walkingApp
+//
+//  Created by John Hur on 2022/01/17.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
